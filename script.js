da6c6929afd1dd991fa71081e2c17d8120b2c086
Artem <aprricod@gmail.com>
2020-09-20 03:16:15 +0200

    First task completed

---
+++ script.js
@@ -0,0 +1,22 @@
+"use strict";
+
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const firstFilm = prompt('Один из последних просмотренных фильмов?', '');
+const firstFilmRate = prompt('На сколько оцените его?', '');
+const secondFilm = prompt('Еще один из последних просмотренных фильмов?', '');
+const secondFilmRate = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[firstFilm] = firstFilmRate;
+personalMovieDB.movies[secondFilm] = secondFilmRate;
+
+console.log(personalMovieDB);
+
